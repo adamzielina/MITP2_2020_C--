f544d64829a816e0cd6f7c6c348c3be8cfa04212
adamzielina <56132235+adamzielina@users.noreply.github.com>
2020-11-14 22:30:40 +0100

    Create zad2.1.cpp

---
+++ Lab_02/zad2.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class circle {
+   public:
+      float radius_;
+};
+
+int main (){
+   circle o1,o2;
+   o1.radius_=4;
+   o2.radius_=5;
+   return 0;
+}
