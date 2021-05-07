6163167db7180c9f662a66c5236bb73b74811c54
adamzielina <56132235+adamzielina@users.noreply.github.com>
2021-05-07 20:41:22 +0200

    Create zadanie.h

---
+++ liceum_pijarow/zadanie.h
@@ -0,0 +1,5 @@
+std::string zad1(std::string slowo);
+std::string zad2(std::string slowo);
+std::string zad3(std::string slowo);
+std::string zad4(std::string slowo);
+std::string zad5(std::string slowo);
